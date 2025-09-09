2fe74578d31af966b20a0984846d8fce4b62f6af
mizznoff <mizznoff+dev@gmail.com>
2025-09-10 02:35:15 +0900

    ステージング環境で動作するようにする

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,9 +1,23 @@
 import { serve } from "@hono/node-server";
+import { serveStatic } from "@hono/node-server/serve-static";
 import { app } from "./app.js";
 
-serve({
-  fetch: app.fetch,
-  port: import.meta.env.VITE_BACKEND_PORT,
-});
+if (process.env.NODE_ENV === "production") {
+  app.use("/assets/*", serveStatic({ root: "./frontend/dist" }));
+  app.use(
+    "*",
+    serveStatic({
+      root: "./frontend/dist",
+      rewriteRequestPath: () => "/index.html",
+    })
+  );
+  serve({
+    fetch: app.fetch,
+    port: import.meta.env.VITE_BACKEND_PORT,
+  });
+  console.log(
+    `Server is running on port: ${import.meta.env.VITE_BACKEND_PORT}`
+  );
+}
 
-console.log(`Server is running on port: ${import.meta.env.VITE_BACKEND_PORT}`);
+export default app;
